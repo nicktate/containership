a7b3377a225d46152ce50bf942ce05dbb826a1a8
Nicholas Tate <ntate22@gmail.com>
2017-02-23 12:36:59 -0500

    Containership CLI V1 Version Protection
    * Upgraded npm module to 4.x
    * Added in logic to only install plugins up to NPM semver version 1.x.x
    * Added in ability to specify exact version of plugin for `update` and
    `add`

--- commands/plugin.js
+++ commands/plugin.js
@@ -10,6 +10,9 @@ const request = require('request');
 const flat = require('flat');
 const async = require('async');
 const mkdirp = require('mkdirp');
+const semver = require('semver');
+
+const PLUGIN_VERSION_CUTOFF = '2.0.0';
 
 module.exports = {
 
@@ -32,7 +35,9 @@ module.exports = {
                     callback: (/*options*/) => {
                         npm.load({
                             prefix: PLUGINS_DIR,
-                            'unsafe-perm': true
+                            'unsafe-perm': true,
+                            force: true,
+                            loglevel: 'silent'
                         }, () => {
                             npm.commands.ls([], { json: true }, (err, data) => {
                                 utils.println([ ['%-40s', 'PLUGIN'], ['%-20s', 'VERSION'] ]);
@@ -74,7 +79,9 @@ module.exports = {
 
                             npm.load({
                                 prefix: PLUGINS_DIR,
-                                'unsafe-perm': true
+                                'unsafe-perm': true,
+                                force: true,
+                                loglevel: 'silent'
                             }, () => {
                                 let plugins = [];
                                 if(_.has(options, 'plugin')) {
@@ -152,20 +159,62 @@ module.exports = {
 
                             npm.load({
                                 prefix: PLUGINS_DIR,
-                                'unsafe-perm': true
+                                'unsafe-perm': true,
+                                force: true,
+                                loglevel: 'silent'
                             }, () => {
                                 async.each(options.plugin, (plugin, callback) => {
+                                    const split = plugin.split('@');
+
+                                    plugin = split[0];
+                                    let specifiedVersion = split.length === 2 ? split[1] : '*';
+
+                                    // validate it conforms to V1 Plugin restriction
+                                    if (specifiedVersion !== '*' && semver.gte(specifiedVersion, PLUGIN_VERSION_CUTOFF)) {
+                                        process.stderr.write(`You cannot specify a version greater than or equal to ${PLUGIN_VERSION_CUTOFF} for V1 Plugins`);
+                                        return process.exit(1);
+                                    }
+
                                     // if authorized plugin, set the source
                                     if(_.has(authorized_plugins, plugin)) {
                                         plugin = authorized_plugins[plugin].source;
                                     }
 
-                                    npm.commands.install([plugin], (err/*, data*/) => {
-                                        if(err) {
-                                            process.stderr.write(`Failed to install plugin: ${plugin}\n`);
+                                    npm.commands.show([`${plugin}@${specifiedVersion}`, 'version'], { loglevel: 'silent' }, (err, data) => {
+                                        if (err) {
+                                            process.stderr.write(`Failed to retrieve plugin versions: ${plugin}\n`);
+                                            process.stderr.write(JSON.stringify(err, null, 2));
+
+                                            return callback();
                                         }
 
-                                        return callback();
+                                        const latestValidVersion = _.reduce(_.keys(data), (accumulator, version) => {
+                                            if (semver.lt(version, PLUGIN_VERSION_CUTOFF)) {
+                                                if (!accumulator || semver.lt(accumulator, version)) {
+                                                    return version;
+                                                }
+                                            }
+
+                                            return accumulator;
+                                        }, null);
+
+                                        if (!latestValidVersion) {
+                                            process.stderr.write('Unable to find valid plugin version to install in the NPM registry');
+                                            process.stderr.write(`Containership V1 Plugins must have npm versions less than ${PLUGIN_VERSION_CUTOFF} and exist in the NPM registry\n`);
+                                            process.stderr.write(`Found plugin versions: ${JSON.stringify(_.keys(data))}`);
+
+                                            return process.exit(1);
+                                        }
+
+                                        plugin = `${plugin}@${latestValidVersion}`;
+
+                                        npm.commands.install([plugin], (err/*, data*/) => {
+                                            if(err) {
+                                                process.stderr.write(`Failed to install plugin: ${plugin}\n`);
+                                            }
+
+                                            return callback();
+                                        });
                                     });
                                 });
                             });
@@ -197,7 +246,9 @@ module.exports = {
 
                             npm.load({
                                 prefix: PLUGINS_DIR,
-                                'unsafe-perm': true
+                                'unsafe-perm': true,
+                                force: true,
+                                loglevel: 'silent'
                             }, () => {
                                 async.each(options.plugin, function(plugin, callback) {
                                     // if authorized plugin, set the source
@@ -239,20 +290,65 @@ module.exports = {
                         request({ url: 'http://plugins.containership.io', json: true }, (err, response) => {
                             let authorized_plugins = {};
 
-                            if(!err && response.statusCode != 200) {
+                            if (!err && response.statusCode === 200) {
                                 authorized_plugins = response.body;
                             }
 
                             npm.load({
                                 prefix: PLUGINS_DIR,
-                                'unsafe-perm': true
+                                'unsafe-perm': true,
+                                force: true,
+                                loglevel: 'silent'
                             }, () => {
+                                const split = options.plugin.split('@');
+
+                                options.plugin = split[0];
+                                options.version = split.length === 2 ? split[1] : '*';
+
+                                // validate it conforms to V1 Plugin restriction
+                                if (options.version !== '*' && semver.gte(options.version, PLUGIN_VERSION_CUTOFF)) {
+                                    process.stderr.write(`You cannot specify a version greater than or equal to ${PLUGIN_VERSION_CUTOFF} for V1 Plugins`);
+                                    return process.exit(1);
+                                }
+
                                 if(_.has(authorized_plugins, options.plugin)) {
                                     options.plugin = authorized_plugins[options.plugin].source;
                                 }
 
-                                process.stdout.write(`Updating plugin: ${options.plugin}\n`);
-                                npm.commands.update([options.plugin], (/*err, data*/) => {});
+                                npm.commands.show([`${options.plugin}@${options.version}`, 'version'], { loglevel: 'silent' }, (err, data) => {
+                                    if (err) {
+                                        process.stderr.write(`Failed to retrieve plugin versions: ${options.plugin}`);
+                                        process.stderr.write(JSON.stringify(err, null, 2));
+
+                                        return process.exit(1);
+                                    }
+
+                                    const latestValidVersion = _.reduce(_.keys(data), (accumulator, version) => {
+                                        if (semver.lt(version, PLUGIN_VERSION_CUTOFF)) {
+                                            if (!accumulator || semver.lt(accumulator, version)) {
+                                                return version;
+                                            }
+                                        }
+
+                                        return accumulator;
+                                    }, null);
+
+                                    if (!latestValidVersion) {
+                                        process.stderr.write('Unable to find valid plugin version to install in the NPM registry');
+                                        process.stderr.write(`Containership V1 Plugins must have npm versions less than ${PLUGIN_VERSION_CUTOFF} and exist in the NPM registry\n`);
+                                        process.stderr.write(`Found plugin versions: ${JSON.stringify(_.keys(data))}`);
+
+                                        return process.exit(1);
+                                    }
+
+                                    options.plugin = `${options.plugin}@${latestValidVersion}`;
+
+                                    npm.commands.install([options.plugin], (err/*, data*/) => {
+                                        if (err) {
+                                            process.stderr.write(JSON.stringify(err, null, 2));
+                                        }
+                                    });
+                                });
                             });
                         });
                     }
